7cc1b9339fbf4f05cc5f5d0d9b62c25aba44ca13
theguywithblacktie <ashish.ash216@gmail.com>
2024-07-06 22:41:27 +0530

    Added more code for experiment parameters and training

--- toxicity_classification/params.py
+++ toxicity_classification/params.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 import torch.optim
+import transformers
 
 experiment_params = {
     "tokenizer_type": "google-bert/bert-base-uncased",
@@ -15,10 +16,31 @@ experiment_params = {
         "kwargs": {
             "weight_decay": 5e-4,
             "lr": 1e-5,
+            "betas": (0.9, 0.999)
+            }
+        },
+    "adamw": {
+        "class": torch.optim.AdamW,
+        "kwargs": {
+            "lr": 1e-6,
+            "betas": (0.9, 0.999),
+            "weight_decay": 1e-5
             }
         }
     },
     "schedulers": {
+        "get-linear-schedule-with-warmup": {
+            "class": transformers.get_linear_schedule_with_warmup,
+            "compute": ["num_warmup_steps", "num_training_steps"],
+            "kwargs": {}
+            },
 
+        "cosine-annealing": {
+            "class": transformers.get_cosine_with_hard_restarts_schedule_with_warmup,
+            "compute": ["num_warmup_steps", "num_training_steps"],
+            "kwargs":{
+                "num_cycles": 1
+                }
+            }
     }
 }
--- toxicity_classification/trainer.py
+++ toxicity_classification/trainer.py
@@ -1,32 +1,7 @@
 import torch
-from torch import nn
-
 import pytorch_lightning as pl
 
-from transformers import AutoModel
-
-
-class ToxicCommentTaggerBERT(nn.Module):
-    def __init__(self, experiment_params: dict):
-        super().__init__()
-        self.transformer = AutoModel.from_pretrained(experiment_params["transformer_name"])
-        self.classification_layers = nn.ModuleList()
-        for index in range(0, len(experiment_params["ffn_params"]["layers"]) - 2):
-            self.classification_layers.append(nn.Linear(experiment_params["ffn_params"]["layers"][index],
-                                                        experiment_params["ffn_params"]["layers"][index + 1]))
-            self.classification_layers.append(experiment_params["ffn_params"]["activation"][index])
-
-            if "dropout" in experiment_params["ffn_params"].keys():
-                self.classification_layers.append(nn.Dropout(experiment_params["ffn_params"]["dropout"][index]))
-
-        self.classification_layers.append(experiment_params["ffn_params"]["activation"][-1])
-        self.classification_layers.append(
-            nn.Linear(experiment_params["ffn_params"]["layers"][-2], experiment_params["ffn_params"]["layers"][-1]))
-
-    def forward(self, input_ids, attention_mask, token_type_ids):
-        _, output_layer = self.transformer(input_ids, attention_mask=attention_mask, token_type_ids=token_type_ids, return_dict=False)
-        logits = self.classification_layers(output_layer)
-        return logits
+from models import ToxicCommentTaggerBERT
 
 
 class ToxicityClassificationTrainer(pl.LightningModule):
@@ -34,7 +9,10 @@ class ToxicityClassificationTrainer(pl.LightningModule):
         super().__init__()
         self.experiment_params = experiment_params
         self.toxicity_model = ToxicCommentTaggerBERT(experiment_params)
-        self.loss_fn = nn.BCELoss()
+        self.lowest_valid_loss = float("inf")
+
+    def common_step(self, batch):
+        return batch["input_ids"], batch["attention_mask"], batch["token_type_ids"], batch["labels"]
 
     def forward(self, input_ids, attention_mask, token_type_ids, labels=None):
         logits = self.toxicity_model(input_ids, attention_mask, token_type_ids)
@@ -43,9 +21,6 @@ class ToxicityClassificationTrainer(pl.LightningModule):
         if labels is not None:
             loss = self.loss_fn(probabilities, labels)
         return loss, probabilities
-
-    def common_step(self, batch):
-        return batch["input_ids"], batch["attention_mask"], batch["token_type_ids"], batch["labels"]
 
     def training_step(self, batch, batch_idx):
         loss, probabilities = self(self.common_step(batch))
@@ -70,4 +45,25 @@ class ToxicityClassificationTrainer(pl.LightningModule):
             scheduler = self.experiment_params["scheduler"]["class"](self.experiment_params["scheduler"]["kwargs"])
             return dict(optimizer=optimizer, lr_scheduler=scheduler)
 
-        return optimizer
+        return dict(optimizer=optimizer)
+
+    def training_epoch_end(self, outputs):
+        """
+        This function is called at the end of each training epoch
+        :param outputs:
+        :return:
+        """
+        loss = sum(output['loss'] for output in outputs)/len(outputs)
+        self.log("Training loss", loss, prog_bar=True, logger=True)
+
+    def validation_epoch_end(self, outputs):
+        """
+        This function is called at the end of
+        :param outputs:
+        """
+        validation_loss = sum(output['loss'] for output in outputs)/len(outputs)
+        self.log("Validation loss", validation_loss, prog_bar=True, logger=True)
+
+        if validation_loss < self.lowest_valid_loss:
+            self.lowest_valid_loss = validation_loss
+            torch.save(self.model.state_dict())
